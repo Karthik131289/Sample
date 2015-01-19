87c6f9c38a92f482aa8474030cc92a6b03bf944e
karthik <karthik131289@gmail.com>
2015-01-19 12:22:31 +0530

    Commit message...

--- src/main/java/com/volante/sample/App.java
+++ src/main/java/com/volante/sample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( Git Goal for Git repo..." );
+        System.out.println( Modified from D drive..." );
     }
 }
