e7b8f4850809480321e684722121ebe57d3c1924
karthik <karthik131289@gmail.com>
2015-01-19 11:40:14 +0530

    Test Commit...

--- src/main/java/com/volante/sample/App.java
+++ src/main/java/com/volante/sample/App.java
@@ -4,10 +4,10 @@ package com.volante.sample;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( Git Goal for Git repo..." );
+        System.out.println( Git Goal for Git repo" );
     }
 }
